83bb0c884dfcf368620efc350ab19bc1a7e13709
Karl Turner <karl.turner5@gmail.com>
2012-03-20 14:43:33 +0000

    fixing errors

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,4 @@
-class MainController < ApplicationController
-
+class MainController < ApplicationController 
  KEY = ENV['KEY']
  SECRET = ENV['SECRET']
 
@@ -11,22 +10,37 @@ class MainController < ApplicationController
 
   end
 
-  # def train
-  #   HTTPARTY.post(API_URL + PATH :query => {:api_key => KEY, :api_secret => SECRET, uids => "Rich", :callback_url => redirect_uri})
+  #manual add code:
+  def add
+    add_url = "http://api.face.com/tags/add"
+    name = params[:name]
+    label = params[:label]
+    add = HTTParty.get(add_url, :query => {
+  											:api_key => KEY, 
+  											:api_secret => SECRET, 
+  											:url => URL+name, 
+  											:x => 100, 
+  											:y => 100, 
+  											:width => 100, 
+  											:height => 100,
+  											:uid => "Rich@karl.turner5", 
+  											:tagger_id => "karl@karl.turner5",
+  											:label => label})
 
-  # 	#expect img url and name
+    response = add.to_hash
+  end
 
-  # end
+
 
   def recognise
   	#http://localhost:3000/main/recognise?name=dicky.jpg   <- request needed
   	name = params[:name]
   	client = Face.get_client(:api_key => KEY, :api_secret => SECRET)
     data = client.faces_detect(:urls => [URL+name])
-    smiling = data.to_hash["photos"][0]["tags"][1]["attributes"]["smiling"]["value"]
-    confidence = data.to_hash["photos"][0]["tags"][1]["attributes"]["smiling"]["confidence"]
 
-    if smiling
+    unless data.to_hash["photos"][0]["tags"] == []
+       smiling = data.to_hash["photos"][0]["tags"][1]["attributes"]["smiling"]["value"]
+       confidence = data.to_hash["photos"][0]["tags"][1]["attributes"]["smiling"]["confidence"]
       @all = ["Smiling?" => smiling, "Confidence?" => confidence]
     else
       @all = "Sorry, something went wrong"
@@ -40,12 +54,28 @@ class MainController < ApplicationController
   end
 
 
-  def callback
+  # def save
+  #   name = params[:name]
+  #   label = params[:label]
+  #   save_url = "http://api.face.com/tags/save.json"
+  #   client = Face.get_client(:api_key => KEY, :api_secret => SECRET)
+  #   data = client.faces_detect(:urls => [URL+name])
+  #   tid = data["photos"][0]["tags"][0]["tid"]
 
-  end
+  #   save= HTTParty.get(save_url, :query => {:api_key => KEY, :api_secret => SECRET, :uid => "Rich@karl.turner5", :tids => tid, :label => label })
+  #   response = save.to_hash
+  #   status = response["status"]
+  #   message = response["message"]
 
-  def redirect_uri
-    "http://" + request.host_with_port + "/callback"
-  end
+  
+  # #training code:
+  #   train_url = "http://api.face.com/faces/train.json"
+  #   train = HTTParty.get(train_url, :query => {:api_key => KEY, :api_secret => SECRET, :uids => "Rich@karl.turner5", :callback_url => redirect_uri})
+  #   train_response = train.to_hash
+  # end
+
+  # def redirect_uri
+  #   "http://" + request.host_with_port + "/callback"
+  # end
 
 end
