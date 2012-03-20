806e2a3fe4ba48593c60db1ce69882d260f5bc75
Karl Turner <karl.turner5@gmail.com>
2012-03-20 11:24:59 +0000

    first commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,52 @@
+class MainController < ApplicationController
+
+ KEY = ""
+ SECRET = ""	
+
+ API_URL = "http://api.face.com/faces/"
+
+
+
+  def index
+
+  end
+
+  # def train
+  #   HTTPARTY.post(API_URL + PATH :query => {:api_key => KEY, :api_secret => SECRET, uids => "Rich", :callback_url => redirect_uri})
+
+  # 	#expect img url and name
+
+  # end
+
+  def recognise
+  	#http://localhost:3000/main/recognise?name=dicky.jpg   <- request needed
+  	name = params[:name]
+  	client = Face.get_client(:api_key => KEY, :api_secret => SECRET)
+    data = client.faces_detect(:urls => ["http://dl.dropbox.com/u/1512097/#{name}"])
+    smiling = data.to_hash["photos"][0]["tags"][1]["attributes"]["smiling"]["value"]
+    confidence = data.to_hash["photos"][0]["tags"][1]["attributes"]["smiling"]["confidence"]
+
+    if smiling
+      @all = ["Smiling?" => smiling, "Confidence?" => confidence]
+    else
+      @all = "Sorry, something went wrong"
+    end
+
+    respond_to do |format|
+      format.xml { render :xml => @all.to_xml }
+      format.html { render :json => @all.to_json }
+      format.json { render :json => @all.to_json }
+    end
+
+  end
+
+
+  def callback
+
+  end
+
+  def redirect_uri
+    "http://" + request.host_with_port + "/callback"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storm::Application.config.secret_token = '1466a02f9c45fd169152469721f6750a34bfea5f135c9416b03160bbc4ae00028157e4ae3bda1dbdfd68b7b4899fb4c8aa14e4568a822a80ad50e7ce670d7cb2'
